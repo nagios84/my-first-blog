0434dec013feb05cef2c31e4c279651748da5ff9
Nagios <nagios84@gmail.com>
2018-09-11 18:20:05 +0300

    Sixth

--- blog/views.py
+++ blog/views.py
@@ -43,4 +43,4 @@ def post_edit(request, id):
             return redirect('post_detail_url', id=post.id)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_new.html', {'form': form})
